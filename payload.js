e9d8cc1328240d4a4a3ff6108463bf353d2788cb
benjamin-wise <143022028+benjamin-wise@users.noreply.github.com>
2026-04-12 13:00:05 -0700

    adding some red outlines

--- payload.js
+++ payload.js
@@ -4,7 +4,7 @@ setTimeout(async () => {
 	let span1 = document.createElement("span");
 	span1.setAttribute("class", "animated-hamburger-ss26__line-2");
 	parent = document.querySelector(".layout-open-menu-base > .animated-hamburger-ss26");
-	parent.setAttribute("style", "--animated-hamburger-width: 64px; --animated-hamburger-height: 32px; background: white; padding: 5px;");
+	parent.setAttribute("style", "--animated-hamburger-width: 64px; --animated-hamburger-height: 32px; background: white; padding: 5px; outline: 5px solid red;");
 	parent.appendChild(span1);
 	
 	let menu = document.querySelector(".zds-carousel-content.zds-carousel-content--vertical");
@@ -32,10 +32,16 @@ setTimeout(async () => {
 	let sidebarBackgroundLeft = document.createElement("div");
 	sidebarBackgroundLeft.setAttribute("class","zds-layout-desktop__left-overlay");
 	sidebarBackgroundLeft.setAttribute("aria-hidden","false");
+	sidebarBackgroundLeft.setAttribute("aria-hidden","false");
 	let sidebarBackgroundRight = document.createElement("div");
 	sidebarBackgroundRight.setAttribute("class","zds-layout-desktop__right-overlay");
 	sidebarBackgroundRight.setAttribute("aria-hidden","false");
 	let whereSidebarShouldBe = document.querySelector(".layout");
 	whereSidebarShouldBe.children[1].insertBefore(sidebarBackgroundLeft,whereSidebarShouldBe.children[1].children[0]);
 	whereSidebarShouldBe.children[1].insertBefore(sidebarBackgroundRight,whereSidebarShouldBe.children[1].children[0]);
+	
+	
+	//navigating to our changed swimwear sites
+	let swimwearMen = document.querySelector("");
+	swimwearMen.setAttribute("href","");
 }, 3000);
